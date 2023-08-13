f7a4b17191f93d6458badc76a38ab60cb6306c84
daniedev <a.danielpf21@gmail.com>
2023-08-13 13:14:05 +0530

    Concept 1 - Thread vs Coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import learncoroutines.makeBulkRequestsUsingCoroutines
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/**
+    try out the functions available in [learncoroutines] here
+ */
+
+fun main() {
+    makeBulkRequestsUsingCoroutines()
 }
---
+++ src/main/kotlin/learncoroutines/ThreadVsCoroutines.kt
@@ -0,0 +1,32 @@
+package learncoroutines
+
+import kotlinx.coroutines.*
+import kotlin.concurrent.thread
+
+/**
+This scenario is a simulation of 100000 users requesting for data to a backend service.
+
+[makeBulkRequestsUsingThread] starts 100000 threads, makes them sleep for 1 second
+and then print the dot on screen (simulation of waiting for data to be retrieved). If you observe, It will take a while for the execution to complete.
+
+Here in [makeBulkRequestsUsingCoroutines], It starts 100000 coroutines and suspends all of them instead of making them sleep, waits for a second
+and prints all the dots. Cost of starting these coroutines is so cheap that its barely noticeable.
+ */
+
+fun makeBulkRequestsUsingThread() {
+    repeat(100000) {
+        thread {
+            Thread.sleep(1000L)
+            print(".")
+        }
+    }
+}
+
+fun makeBulkRequestsUsingCoroutines() = runBlocking {
+    repeat(100000) {
+        launch {
+            delay(1000L)
+            print(".")
+        }
+    }
+}
